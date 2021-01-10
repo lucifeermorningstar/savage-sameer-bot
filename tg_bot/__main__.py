0125513882efc864065883ae9126dcbbaeac5cd1
TEAM-PIKACHU-INDIA <73126271+TEAM-PIKACHU-INDIA@users.noreply.github.com>
2021-01-10 18:28:50 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -167,9 +167,8 @@ def start(bot: Bot, update: Update, args: List[str]):
                             text="☺Help👀",
                             url="https://t.me/?start=help")
                     ]]
-         update.effective_message.reply_photo(MEIKO_IMG,
-                PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=buttons)
+                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
+                 parse_mode=ParseMode.MARKDOWN, reply_markup=buttons)
 
     else:
         update.effective_message.reply_text("Heya! Am Awake 😁")
